e25ac3684bdc119088e2252bb2202fdf0d0f6828
Coleman Cox <colezane82@gmail.com>
2015-07-21 00:57:25 -0500

    Having trouble understanding the js part of things

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 var source   = document.querySelector("#entry-template").innerHTML;
 var template = Handlebars.compile(source);
 
-var onLoad = function(data){
-    debugger;
+var onLoad = function(data) {
+    var s = template(data);
+    var t =
+
+
 };
-
-
 loadEtsy("dogs", onLoad);
